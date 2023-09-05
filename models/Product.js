d8abac37dc6b71f1ce4cfd9a6af7a01e86bb076e
Andres Fernandez <94069766+Afernan413@users.noreply.github.com>
2023-09-04 21:55:39 -0400

    W

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+import mongoose, {model, models, Schema} from "mongoose";
+
+const ProductSchema = new Schema({
+    title: {type:String, required: true},
+    description: String,
+    price : {type:Number, required:true},
+    images : [{type:String}],
+    category : {type:mongoose.Schema.Types.ObjectId, required:false, ref:'Category'},
+    properties : {type:Object}
+});
+
+export const Product = models?.product || model('product', ProductSchema);
+
